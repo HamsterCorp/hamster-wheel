c55ef256f49c6d91a4c3d5ee3229e3323b702b0f
Justin Uzoije <justinuzoije@gmail.com>
2017-04-17 11:38:49 -0400

    Change port to 3001

--- hamster.js
+++ hamster.js
@@ -153,6 +153,6 @@ app.get('/logout', function(req, resp) {
   resp.redirect('/');
 });
 
-app.listen(3000, function() {
-  console.log('Listening on port 3000.');
+app.listen(3001, function() {
+  console.log('Listening on port 3001.');
 });
